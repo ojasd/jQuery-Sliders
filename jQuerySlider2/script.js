598ef6741a4cb08c22eac1a3bf163f83893dfaad
Ojas Deshpande <ojas25@gmail.com>
2016-06-15 17:24:40 -0400

    Push

---
+++ jQuerySlider2/script.js
@@ -0,0 +1,34 @@
+// Code goes here
+
+$(function () {
+    $(".slider").each(function () {
+        var begin = $(this).data("begin"),
+            end = $(this).data("end");
+
+        $(this).slider({
+            range: "min",
+            min: begin,
+            max: end,
+            animate: true,
+            slide: function (event, ui) {
+                //update text box quantity
+                var slideramount = ("#" + $(this).attr("id") + "_amount");
+                $(slideramount).val(ui.value);
+            }
+        })
+
+        //initialise text box quantity
+        var slideramount = ("#" + $(this).attr("id") + "_amount");
+        $(slideramount).val($(this).slider("value"));
+ 
+    })
+
+
+    
+    //When radio buttons are changed, update slider 
+    $('[name="[temperature]"]').change(function () {
+          var value = this.value;
+          $('#temperature').slider("value", value);
+    });
+
+});
